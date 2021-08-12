ae3b775617ca65f794b36b573b197b26813002d8
Sanjib <sanjib12sinha@gmail.com>
2021-08-12 13:08:31 +0530

    adding image

--- directoryIterator.php
+++ directoryIterator.php
@@ -14,17 +14,21 @@
           </h1>
           <p id="body">
           <?php
-            $dir = new DirectoryIterator('all-files/images');
+            $dir = new DirectoryIterator('all-files/images/');
             foreach ($dir as $key => $file) {
                 //var_dump($file);
                 if ($file->isFile()) {
                 echo $key . ': ' . $file->getPathname() . '<br>';
-                    // $files[] = clone $file;
-                }
-            }
-            // echo $files[1]->getFilename();
+                    $files[] = clone $file;
+                    
+               }
+            } ?>
+<img src="all-files/images/<?php
 
-            ?>
+echo $files[1]->getFilename();
+
+?>" alt="">
+            
           </p>
         
       </div>
